3b26f401c9b7a72952c1bb40888da6f35521829d
Zhang Jianhua <jeff@ZhangdeMacBook-Pro.local>
2013-11-13 15:24:11 +0800

    Initial Commit

---
+++ psobase/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  psobase
+//
+//  Created by Zhang Jianhua on 13-11-13.
+//  Copyright (c) 2013年 Zhang Jianhua. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
